09cb23fd45c8305c4a9515f64914f1a3aa297532
Caio Silva <bladellano@gmail.com>
2019-06-27 22:25:17 -0300

    Ajustando fancybox e slick

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/menu.css">
+	<link rel="stylesheet" href="css/jquery.fancybox-1.3.4.css">
+	<link rel="stylesheet" href="css/slick.css">
 	<link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet">
 </head>
 <body>
@@ -69,63 +71,36 @@
 
 		<section class="content-site">
 			<div class="container">
-				<div class="row">
-					<div class="col-xs-12">
-						<h1 class="text-center">Últimos Eventos</h1>
-						<p class="text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make</p>
-						<br>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-sm-3">
-						<div class="thumbnail">
-							<img src="img/foto1.jpg" alt="">
-							<div class="caption text-center">
-								<h3>Contrary to popular belief</h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-sm-3">
-						<div class="thumbnail">
-							<img src="img/foto1.jpg" alt="">
-							<div class="caption text-center">
-								<h3>Contrary to popular belief</h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-sm-3">
-						<div class="thumbnail">
-							<img src="img/foto1.jpg" alt="">
-							<div class="caption text-center">
-								<h3>Contrary to popular belief</h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-sm-3">
-						<div class="thumbnail">
-							<img src="img/foto1.jpg" alt="">
-							<div class="caption text-center">
-								<h3>Contrary to popular belief</h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+
+				<h1 class="text-center">Últimos Eventos</h1>	
+ 
+
+				<div class="slick">
+					<div><a rel="albuns1" class="fancy" href="img/eventos/evento1.jpeg"><img class="thumnails" src="img/eventos/evento1.jpeg"></a></div>
+					<div><a rel="albuns1" class="fancy" href="img/eventos/evento2.jpeg"><img class="thumnails" src="img/eventos/evento2.jpeg"></a></div>
+					<div><a rel="albuns1" class="fancy" href="img/eventos/evento3.jpeg"><img class="thumnails" src="img/eventos/evento3.jpeg"></a></div>
+					<div><a rel="albuns1" class="fancy" href="img/eventos/evento4.jpeg"><img class="thumnails" src="img/eventos/evento4.jpeg"></a></div>
+					<div><a rel="albuns1" class="fancy" href="img/eventos/evento5.jpeg"><img class="thumnails" src="img/eventos/evento5.jpeg"></a></div>
+					<!-- <div><a class="fancy" href="img/eventos/evento6.jpeg"><img class="thumnails" src="img/eventos/evento6.jpeg"></a></div> -->
+					<div><a rel="albuns1" class="fancy" href="img/eventos/evento7.jpeg"><img class="thumnails" src="img/eventos/evento7.jpeg"></a></div>
+					<div><a rel="albuns1" class="fancy" href="img/eventos/evento8.jpeg"><img class="thumnails" src="img/eventos/evento8.jpeg"></a></div>
+					<div><a rel="albuns1" class="fancy" href="img/eventos/evento9.jpeg"><img class="thumnails" src="img/eventos/evento9.jpeg"></a></div>
+					<div><a rel="albuns1" class="fancy" href="img/eventos/evento10.jpeg"><img class="thumnails" src="img/eventos/evento10.jpeg"></a></div>
+				    
+				</div><!-- slick -->
+<h1 class="text-center">Últimos Videos</h1>	
+			</div><!--container--> 
 		</section>
 
 		<section class="img-site">
 			<div class="container">
-						<div class="row">
-				<div class="col-xs-12">
-					<h1 class="text-center">Membros PFC</h1>
-					<p class="text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make</p>
-					<br>
+				<div class="row">
+					<div class="col-xs-12">
+						<h1 class="text-center">Membros PFC</h1>
+						<p class="text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make</p>
+						<br>
+					</div>
 				</div>
-			</div>
 				<div class="row">						 
 					<div class="col-sm-3">
 						<div class="thumbnail">
@@ -224,8 +199,11 @@
 			</div>
 		</section>
 
+		<script src="js/jquery-1.4.3.min.js"></script>
+		<script src="js/jquery.fancybox-1.3.4.js"></script>
 		<script src="js/jquery.min.js"></script>
 		<script src="js/bootstrap.js"></script>
+		<script src="js/slick.js"></script>
 		<script src="js/functions.js"></script>
 	</body>
 	</html>
--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,37 @@
-$(function(){
+$( document ).ready(function() {
 
-
+	//ALTERA O FUNDO DO NAVMENU
 	$(window).scroll(function(event) {
 		if($(this).scrollTop() > 0){
-			// $('.topo').fadeIn();
-			console.log('entrou..')
-			// $('.menu').fadeOut();
 			$('.menu').addClass('active');
 		} else {
-			// $('.topo').fadeOut();
-			console.log('saiu..')
 			$('.menu').fadeIn();
 			$('.menu').removeClass('active');
 		}
 	});
+
+	//Plugin slick
+	$('.slick').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		arrows:false,
+		responsive: [    
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				arrows:false
+
+			}
+		}]
+	});
+
+
+	//Plugin FancyBox
+ 	$j('a.fancy').fancybox();
+		 
 
 });
